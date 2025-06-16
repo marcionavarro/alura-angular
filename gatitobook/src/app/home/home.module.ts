0f216e921410df872252c283be602ca3e9ae5279
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-06-16 20:20:36 -0300

    02-Angular boas práticas em arquiteturas e formulários[02-Login] Finalizado

--- gatitobook/src/app/home/home.module.ts
+++ gatitobook/src/app/home/home.module.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home.component';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { MensagemModule } from '../componentes/mensagem/mensagem.module';
 
 @NgModule({
   declarations: [HomeComponent, LoginComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, FormsModule, MensagemModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
