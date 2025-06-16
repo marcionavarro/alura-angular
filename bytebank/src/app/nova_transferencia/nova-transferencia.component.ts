a633526d75ce61d230e978fb133e5e517af7b2be
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-06-16 16:30:53 -0300

    Angular explorando o framework[06-Comunicação HTTP] Finalizado

--- bytebank/src/app/nova_transferencia/nova-transferencia.component.ts
+++ bytebank/src/app/nova_transferencia/nova-transferencia.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { TransferenciaService } from '../services/transferencia.service';
+import { Transferencia } from '../models/transferencia.model';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -11,10 +13,21 @@ export class NovaTransferenciaComponent {
   valor: number;
   destino: number;
 
+  constructor(private service: TransferenciaService) {}
+
   transferir() {
     console.log('Solicitada nova transferência');
-    const valorEmitir = { valor: this.valor, destino: this.destino };
-    this.aoTranferir.emit(valorEmitir);
+    const valorEmitir: Transferencia = {
+      valor: this.valor,
+      destino: this.destino,
+    };
+    this.service.adicionar(valorEmitir).subscribe(
+      (resultado) => {
+        console.log(resultado);
+        this.limparCampos();
+      },
+      (error) => console.error(error)
+    );
     this.limparCampos();
   }
 
