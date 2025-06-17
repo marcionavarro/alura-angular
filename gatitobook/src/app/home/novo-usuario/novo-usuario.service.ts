31482c90ebc3f4ceed865d0fa5361714fa29135d
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-06-17 01:08:11 -0300

    02-Angular boas práticas em arquiteturas e formulários[04-Validação de formulário assíncrono] Finalizado

--- gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { NovoUsuario } from './novo-usuario';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,5 +10,9 @@ export class NovoUsuarioService {
 
   cadastraNovoUsuario(novoUsuario: NovoUsuario) {
     return this.http.post('http://localhost:3000/user/signup', novoUsuario);
+  }
+
+  verificaUsuarioExistente(nomeUsuario: string) {
+    return this.http.get(`http://localhost:3000/user/exists/${nomeUsuario}`);
   }
 }
