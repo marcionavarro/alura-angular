8ea1bc6f9317a2eefee8a64461bce57521e03974
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-06-18 18:21:35 -0300

    03-Angular controle o fluxo de navegação[05-Novo animal] Finalizado

--- gatitobook/src/app/animais/animais-routing.module.ts
+++ gatitobook/src/app/animais/animais-routing.module.ts
@@ -3,12 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 import { ListaAnimaisResolver } from './lista-animais/lista-animais.resolver';
+import { NovoAnimalComponent } from './novo-animal/novo-animal.component';
+import { AutenticacaoGuard } from '../autenticacao/autenticacao.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: ListaAnimaisComponent,
     resolve: { animais: ListaAnimaisResolver },
+  },
+  {
+    path: 'novo',
+    component: NovoAnimalComponent,
+    canLoad: [AutenticacaoGuard],
   },
   { path: ':animalId', component: DetalheAnimalComponent },
 ];
--- gatitobook/src/app/componentes/cabecalho/cabecalho.component.ts
+++ gatitobook/src/app/componentes/cabecalho/cabecalho.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class CabecalhoComponent implements OnInit {
   user$ = this.usuarioService.retornaUsuario();
 
-  constructor(private usuarioService: UsuarioService, private router: Router) {}
+  constructor(private usuarioService: UsuarioService, public router: Router) {}
 
   logout() {
     this.usuarioService.logout();
