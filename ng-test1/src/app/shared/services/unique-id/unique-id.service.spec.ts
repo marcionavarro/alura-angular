a7ae2fb4ef1d76ca66ec696209d6e325c390baf7
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-06-27 18:19:44 -0300

    01-Angular testes automatizados com Jasmine e Karma[02-Lapidando nossos testes] Finalizado

--- ng-test1/src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ ng-test1/src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,9 +1,41 @@
 import { UniqueIdService } from './unique-id';
 
 describe(UniqueIdService.name, () => {
-  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} generateUniqueIdWithPrefix should generate id when called with prefix`, () => {
-    const service = new UniqueIdService();
+  let service: UniqueIdService = null
+
+  beforeEach(() => {
+    service = new UniqueIdService();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} 
+    generateUniqueIdWithPrefix should generate id when called with prefix`, () => {
     const id = service.generateUniqueIdWithPrefix('app');
-    expect(id).toContain('app-');
+    expect(id.startsWith('app-')).toBeTrue();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} 
+    should not generate duplicate IDs when called multiple times`, () => {
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
+  });
+
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} 
+    should return number of generatedIds when called`, () => {
+    service.generateUniqueIdWithPrefix('app');
+    service.generateUniqueIdWithPrefix('app');
+    expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} 
+    should throw when called with empty`, () => {
+    const emptyValues = [null, undefined, '0', '1'];
+    emptyValues.forEach(emptyValue => {
+      expect(() => service.generateUniqueIdWithPrefix(emptyValue))
+        .withContext(`Empty value ${emptyValue}`)
+        .toThrow();
+    })
   });
 });
--- ng-test1/src/app/shared/services/unique-id/unique-id.ts
+++ ng-test1/src/app/shared/services/unique-id/unique-id.ts
@@ -4,16 +4,17 @@ import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class UniqueIdService {
   private numberOfGeneratedIds = 0;
+  private validId = /^[A-Za-z]+[\w\-\:\.]*$/;
 
   public generateUniqueIdWithPrefix(prefix: string): string {
-    if (!prefix) throw Error('Prefix ca not be empty');
+    if (!prefix || !this.validId.test(prefix)) throw Error('Prefix can not be empty');
 
     const uniqueId = this.generateUniqueId();
     this.numberOfGeneratedIds++;
     return `${prefix}-${uniqueId}`;
   }
 
-  public getNumnerOfGeneratedUniqueIds() {
+  public getNumberOfGeneratedUniqueIds() {
     return this.numberOfGeneratedIds;
   }
 
