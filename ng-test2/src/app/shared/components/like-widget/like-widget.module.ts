47916177cdef400122e8f68c5d9814d5d89feda1
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-07-02 23:22:12 -0300

    02-Angular avançando com testes automatizados[03-Lidando com eventos de UI] Finalizado

--- ng-test2/src/app/shared/components/like-widget/like-widget.module.ts
+++ ng-test2/src/app/shared/components/like-widget/like-widget.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { UniqueIdService } from '../../services/unique-id/unique-id.service';
 import { LikeWidgetComponent } from './like-widget.component';
+import { ActionDirectiveModule } from '../../directives/action/action.module';
 
 @NgModule({
   declarations: [LikeWidgetComponent],
   imports: [
     CommonModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ActionDirectiveModule
   ],
   exports: [LikeWidgetComponent],
   providers: [UniqueIdService]
 })
-export class LikeWidgetModule {}
+export class LikeWidgetModule { }
