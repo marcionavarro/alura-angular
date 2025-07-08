6b9c84dd6a73da4c41a3e9ad5250ccea0d1941f9
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-07-07 23:48:00 -0300

    02-Angular avançando com testes automatizados[04-Cuidados com a abstração do Angular] Finalizado

--- ng-test2/src/app/app.component.ts
+++ ng-test2/src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoBoardService } from './shared/components/photo-board/services/photo-board.service';
+import { Observable } from 'rxjs';
+import { Photo } from './shared/components/photo-board/interfaces/photo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular testing';
-  public likes = 0;
+  public photos$: Observable<Photo[]>;
 
-  public like(): void {
-    this.likes++;
+  constructor(private service: PhotoBoardService) { }
+  
+  public ngOnInit(): void {
+    this.photos$ = this.service.getPhotos();
   }
 }
---
+++ ng-test2/src/app/shared/components/photo-board/photo-board.component.spec.ts
@@ -0,0 +1,44 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PhotoBoardComponent } from './photo-board.component';
+import { PhotoBoardModule } from './photo-board.module';
+import { Photo } from './interfaces/photo';
+import { SimpleChange, SimpleChanges } from '@angular/core';
+
+function buildPhotoList(): Photo[] {
+    const photos: Photo[] = [];
+    for (let i = 0; i < 8; i++) {
+        photos.push({
+            id: i + 1,
+            url: '',
+            description: ''
+        })
+    }
+
+    return photos;
+}
+
+describe(PhotoBoardComponent.name, () => {
+    let fixture: ComponentFixture<PhotoBoardComponent>
+    let component: PhotoBoardComponent;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [PhotoBoardModule]
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(PhotoBoardComponent);
+        component = fixture.componentInstance;
+    });
+
+    it(`Should display rows and columns when (@Input photos) has value`, () => {
+        component.photos = buildPhotoList();
+        const change: SimpleChanges = {
+            photos: new SimpleChange([], component.photos, true)
+        };
+        component.ngOnChanges(change);
+        fixture.detectChanges();
+        expect(component.rows.length).withContext('Number of rows').toBe(2);
+        expect(component.rows[0].length).withContext('Number of columns from the first row').toBe(4);
+        expect(component.rows[1].length).withContext('Number of columns from the second row').toBe(4);
+    })
+})
