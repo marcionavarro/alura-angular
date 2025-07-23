5222bace9114d24f85ac8dcd3accd19b634647e0
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-07-23 00:11:06 -0300

    01-Angular e RxJS programação reativa[04-Operadores de filtro] Finalizado

--- alura_angular_rxjs/byte-bank-broker/src/app/acoes/acoes.component.ts
+++ alura_angular_rxjs/byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { AcoesService } from './acoes.service';
-import { switchMap, tap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs/operators';
 import { merge, Observable } from 'rxjs';
 import { Acoes } from './modelo/acoes';
+
+const ESPERA_DIGITACAO = 300;
 
 @Component({
   selector: 'app-acoes',
@@ -24,8 +26,13 @@ export class AcoesComponent {
     );
 
     this.filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+      debounceTime(ESPERA_DIGITACAO),
       tap(() => { console.log("Fluxo do filtro") }),
-      switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado))
+      tap(console.log),
+      filter((valorDigitado) => valorDigitado.length >= 3 || !valorDigitado.length),
+      distinctUntilChanged(),
+      switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado)),
+      tap(console.log)
     );
 
     this.acoes$ = merge(this.todasAcoes$, this.filtroPeloInput$);
