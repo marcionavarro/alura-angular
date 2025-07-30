be31a4d571ac8cc201bfb5bde5a136b7122911c9
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-07-29 21:10:14 -0300

    1-Protractor testando sua aplicação de ponta a ponta[02-Interagindo com elementos] Finalizado

--- protractor/e2e/home.e2e-spec.ts
+++ protractor/e2e/home.e2e-spec.ts
@@ -1,9 +1,34 @@
-import { browser } from 'protractor';
+import { browser, by, element, protractor } from 'protractor';
 
 describe('Home Page', () => {
     it('Should navigate to user profile', async () => {
         await browser.get(`${browser.baseUrl}/#/user/flavio`);
         const title = await browser.getTitle();
         expect(title).toBe('Timeline');
+    });
+
+    it('Should display a list of photos', async () => {
+        await browser.get(`${browser.baseUrl}/#/user/flavio`);
+        const list = element.all(by.css('.photo'));
+        const photoListSize = await list.count();
+        expect(photoListSize).toBeGreaterThan(0);
+    });
+
+    it('Should navigate to photo detail when photo navigation is triggered', async () => {
+        await browser.get(`${browser.baseUrl}/#/user/flavio`);
+        const firstElement = element.all(by.css('.photo')).first();
+        // await firstElement.sendKeys(protractor.Key.ENTER); // Métoddo quando o webdriver antigo
+        await firstElement.click(); // Metodo atualizado
+        const title = await browser.getTitle();
+        expect(title).toBe('Photo detail');
+    });
+
+    it('Should list one item when filtering by word "farol"', async () => {
+        await browser.get(`${browser.baseUrl}/#/user/flavio`);
+        const searchInput = element(by.css('ap-search input[type=search]'));
+        await searchInput.sendKeys('farol');
+        const list = element.all(by.css('.photo'));
+        const photoListSize = await list.count();
+        expect(photoListSize).toBe(1);
     })
 })
